cca7554f16057d9b9b9195f99bbb36a7b6d0e0a6
Sjölund <eric.sjolund@sogeti.com>
2025-09-23 13:50:14 +0200

    Fully working code

--- Klósettröð.py
+++ Klósettröð.py
@@ -1,4 +1,12 @@
 #Klósettröð
-
-n=int(input())
-
+n = int(input())
+v = list(map(int, input().split(" ")))
+e=[0]*n
+pos = 0
+for i in range(n):
+    for j in range(len(v)):
+        if v[j] == max(v):
+            e[i] = j+1
+            v[j] = 0
+            break
+print(' '.join(map(str, e)))

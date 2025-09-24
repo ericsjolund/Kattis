006d74ef46f1b2b2d893ce3908f684e67ba64ba8
Sjölund <eric.sjolund@sogeti.com>
2025-09-24 11:43:07 +0200

    functional solution. Problem: time limit exceeded

---
+++ 100 meter dash.py
@@ -0,0 +1,58 @@
+import math
+
+a=int(input())
+points = []
+points = [(0.0, 0.0, 0.0)] + points
+
+for _ in range(a):
+    x,y,t=map(float, input().split(" "))
+    points.append((x,y,t))
+
+
+segs = []
+
+for i in range(1,len(points)):
+
+    x0,y0,t0 = points[i-1]
+    x1,y1,t1 = points[i]
+    
+    l = math.hypot(x1-x0, y1-y0)
+    dt = t1-t0
+
+    if dt <= 0:
+        continue
+    v = l/dt
+    segs.append((l,dt,v))
+
+    ans = float("inf")
+
+    lwin =0.0
+    twin = 0.0
+    s = 0
+
+    for j in range(len(segs)):
+        l,dt,v = segs[j]
+
+        lwin += l
+        twin += dt
+
+        while s <= j and lwin - segs[s][0] >= 100 - 1e-15:
+            lwin -= segs[s][0]
+            twin -= segs[s][1]
+            s += 1
+
+        lbl = lwin- l
+        tbl = twin - dt
+        n = 100 - lbl
+
+        if n > 0:
+            t = min(n,l)
+            if t >= n - 1e-15:
+                ans  = min(ans, tbl + t / v)
+
+        if lwin >= 100 - 1e-15:
+            surp = lwin - 100
+            vstart = segs[s][2]
+            ans = min(ans, twin-surp/vstart)
+
+print(ans, flush=True)

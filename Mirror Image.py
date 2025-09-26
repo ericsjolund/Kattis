5e13926119bc3eec0218f133e1d25e742fd3b76a
erisjolund <eric.sjolund@sogeti.com>
2025-09-26 10:03:36 +0200

    Mirror image finished

--- Mirror Image.py
+++ Mirror Image.py
@@ -1,1 +1,7 @@
-
+T=int(input())
+for i in range(T):
+    R,C=map(int,input().split(" "))
+    L = [input().rstrip('\n') for _ in range(R)]
+    print(f"Test {i+1}")
+    for r in range(R - 1, -1, -1):
+        print(L[r][::-1])
